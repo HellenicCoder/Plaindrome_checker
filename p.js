babd5836773bc6ae3ab35fbad8c202c7d6573cff
Yahya Nebgui <NebguiYahya26@gmail.com>
2024-09-27 13:21:12 +0100

    first commit

---
+++ p.js
@@ -0,0 +1,23 @@
+const txtInput = document.querySelector(".inputs input"),
+checkBtn = document.querySelector(".inputs button"),
+infoTxt = document.querySelector(".info-txt");
+
+let filterInput;
+
+checkBtn.addEventListener("click", () => {
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+   if(filterInput != reverseInput){
+   return infoTxt.innerHTML = `no, <span>'${txtInput.value}'</span> isn't a palindrome!`
+   } 
+   return infoTxt.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a palindrome!`
+})
+
+txtInput.addEventListener("keyup", () => {
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if(filterInput){
+       return checkBtn.classList.add("active");
+    }
+    infoTxt.style.display = "none";
+    return checkBtn.classList.remove("active");
+})
